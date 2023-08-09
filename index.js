15e270d39f551259e035a770d1e97c841605c8bc
achowdhary19 <82773599+achowdhary19@users.noreply.github.com>
2023-08-09 17:06:56 -0500

    rebranded portfolio
    Took out a bunch of projects, brought images of all the projects to the home page

--- index.js
+++ index.js
@@ -15,3 +15,11 @@ expandableTopItems.forEach(function(topItem) {
     return false;
   });
 });
+
+
+var popupWindow = null;
+function positionedPopup(url,winName,w,h,t,l,scroll){
+    settings =
+    'height='+h+',width='+w+',top='+t+',left='+l+',scrollbars='+scroll+',resizable'
+    popupWindow = window.open(url,winName,settings)
+}
