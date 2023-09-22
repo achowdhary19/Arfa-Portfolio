64f6b1f74f8b183b1b16ba2b6f604a87ad6dbba3
achowdhary19 <82773599+achowdhary19@users.noreply.github.com>
2023-09-22 17:22:04 +0200

    Added  dollhouse page and providence page

--- index.js
+++ index.js
@@ -23,3 +23,33 @@ function positionedPopup(url,winName,w,h,t,l,scroll){
     'height='+h+',width='+w+',top='+t+',left='+l+',scrollbars='+scroll+',resizable'
     popupWindow = window.open(url,winName,settings)
 }
+
+let index;
+let slideIndex = 1;
+
+//W3 
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slide");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  // dots[slideIndex-1].className += " active";
+}
