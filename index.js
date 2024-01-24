773177aa44ab8dccef9c54693cdbdce48e422498
achowdhary19 <82773599+achowdhary19@users.noreply.github.com>
2024-01-24 17:38:49 -0600

    added content to providence page
    made some adjustments for mobile

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var expandableTopItems = document.querySelectorAll(".topItem"); 
+var expandableTopItems = document.querySelectorAll(".topItem");
 
-expandableTopItems.forEach(function(topItem) {
-   /* buttons are generated on init, to support no JS and have the submenus displayed by default */
-  topItem.innerHTML=`<button type=button aria-expanded="false">${topItem.textContent}</button>`; //insert a button in the spans
-  topItem.nextElementSibling.hidden=true; //hide submenu by default
-  var btn=topItem.firstElementChild;
+expandableTopItems.forEach(function (topItem) {
+  /* buttons are generated on init, to support no JS and have the submenus displayed by default */
+  topItem.innerHTML = `<button type=button aria-expanded="false">${topItem.textContent}</button>`; //insert a button in the spans
+  topItem.nextElementSibling.hidden = true; //hide submenu by default
+  var btn = topItem.firstElementChild;
   /* add listener on each button to implement the behavior on click */
-  btn.addEventListener("click", function(e) {
+  btn.addEventListener("click", function (e) {
     let expanded = this.getAttribute("aria-expanded") === "true" || false;
     this.setAttribute("aria-expanded", !expanded);
     let submenu = this.parentNode.nextElementSibling;
@@ -18,10 +18,10 @@ expandableTopItems.forEach(function(topItem) {
 
 
 var popupWindow = null;
-function positionedPopup(url,winName,w,h,t,l,scroll){
-    settings =
-    'height='+h+',width='+w+',top='+t+',left='+l+',scrollbars='+scroll+',resizable'
-    popupWindow = window.open(url,winName,settings)
+function positionedPopup(url, winName, w, h, t, l, scroll) {
+  settings =
+    'height=' + h + ',width=' + w + ',top=' + t + ',left=' + l + ',scrollbars=' + scroll + ',resizable'
+  popupWindow = window.open(url, winName, settings)
 }
 
 let index;
@@ -42,14 +42,39 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("slide");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  if (n > slides.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex - 1].style.display = "block";
   // dots[slideIndex-1].className += " active";
+}
+
+let enlarged = 0;
+function toggleEnlargeImg(img) {
+  if (enlarged === 0) {
+    img.style.transform = "scale(1.75)";
+    //remove the next two lines if you dont want it to be centered and just want enlarge 
+
+    img.classList.add("center");
+    img.classList.remove("sketch");
+
+    img.style.transition =
+      "transform 0.2s ease";
+    enlarged = 1;
+  }
+  else {
+    img.style.transform = "scale(1)";    //remove the next two lines if you dont want it to be centered and just want enlarge 
+
+    img.classList.remove("center");
+    img.classList.add("sketch");
+    img.style.transition =
+      "transform 0.2s ease";
+    enlarged = 0;
+  }
+  return enlarged;
 }
